cd95148352d50bb2895407faffe944f31557aebe
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2025-03-26 23:21:44 +0600

    'commited'

--- src/boilerplate/index.js
+++ src/boilerplate/index.js
@@ -6,11 +6,22 @@ import metadata from './block.json';
 import Edit from './edit';
 import save from './save';
 registerBlockType(metadata.name, {
+	// icon: (
+	// 	<svg
+	// 		version="1.1"
+	// 		viewBox="0 0 500 500"
+	// 		preserveAspectRatio="xMidYMid meet"
+	// 	>
+	// 		<circle cx="250" cy="250" r="200" />
+	// 	</svg>
+	// ),
+
 	icon: {
 		src: 'text-page',
 		background: '#f03',
 		foreground: '#fff',
 	},
+
 	edit: Edit,
 	save,
 });
