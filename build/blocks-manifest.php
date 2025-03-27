edb61dc9dbfe52d020aba81c10a072714d29df49
Nayem Miah <nayemmiah.cu@gmail.com>
2025-03-27 11:15:11 +0600

    'comiited'

---
+++ build/blocks-manifest.php
@@ -0,0 +1,30 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'boilerplate' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'blocks-course/text-box',
+		'version' => '0.1.0',
+		'title' => 'Text-Box',
+		'category' => 'Text',
+		'description' => 'A box of text.',
+		'keywords' => array(
+			'text',
+			'text-box',
+			'paragraph',
+			'content'
+		),
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'text-box',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ build/boilerplate/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7ff2295c5996bb220062');
